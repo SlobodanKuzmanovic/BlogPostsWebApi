ede7c404a90f1b72e6f1da52f3b928d2c999caae
SlobodanKuzmanovic <slobodankuzmanovic@yahoo.com>
2020-04-06 21:59:33 +0200

    lunch settings BlogPostAPi added

---
+++ BlogPostsWebApi/Controllers/BlogPost.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPostsWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/posts")]
+    public class BlogPost
+    {
+
+
+        [HttpGet]
+        [Route("~/api/posts")]
+        public JsonResult Get()
+        {
+            return new JsonResult("Get");
+        }
+
+        [HttpGet]
+        [Route("~/api/posts/:slug")]
+        public JsonResult Get(string slug)
+        {
+            return new JsonResult("Get with slug: " + slug);
+        }
+
+
+        [HttpGet]
+        [Route("welcome")]
+        public JsonResult Welcome()
+        {
+            return new JsonResult("Welcome");
+        }
+    }
+}
