fe6067398f7e6c79c7804fce74ababbc265da235
SlobodanKuzmanovic <slobodankuzmanovic@yahoo.com>
2020-04-06 20:30:08 +0200

    Common models, DAL and BL

---
+++ CommonLayer/SingleBlogPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class SingleBlogPost
+    {
+        public string slug { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string body { get; set; }
+        public List<string> tagList { get; set; }
+        public string createdAt { get; set; }
+        public string updatedAt { get; set; }
+
+        public SingleBlogPost()
+        {
+            tagList = new List<string>();
+        }
+    }
+}
---
+++ CommonLayer/rm_MultipleBlogPosts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class rm_MultipleBlogPosts
+    {
+    }
+}
