6a4dcab008cd048eef1d6d319f3675b538fd35db
Slobodan Kuzmanovic <slobodan.kuzmanovic@transcore.com>
2021-09-11 23:55:55 +0200

    Swagger implemented

--- BlogPostsWebApi/Startup.cs
+++ BlogPostsWebApi/Startup.cs
@@ -26,11 +26,18 @@ namespace BlogPostsWebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blog Post Web API");
+                });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
